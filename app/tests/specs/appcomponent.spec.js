affb0b0bdc3f88684063275cfc343fb6ebbb20ce
Chuck <chuck@pm-mac1.local>
2018-05-31 17:32:01 -0400

    beginnings of unit tests

---
+++ app/tests/specs/appcomponent.spec.js
@@ -0,0 +1,14 @@
+/// <reference path="""../../../
+describe('In the file app.component', () => {
+  describe('the AppComponent\'s', () => {
+    describe('constructor', () => {
+
+    });  
+    describe('getBalance method', () => {
+
+    });  
+    describe('makePayment method', () => {
+
+    });  
+  });
+});
